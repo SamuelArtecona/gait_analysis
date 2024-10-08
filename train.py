d3c6ab74caf03c4e4b2aa92648ca0e3b6c854b35
jeh15 <jeh15@fsu.edu>
2024-10-08 17:24:34 -0400

    Testing out reward function discussed during October 8ths meeting.

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import optax
 import wandb
 import orbax.checkpoint as ocp
 
-from src.envs import unitree_go2_energy as unitree_go2
+from src.envs import unitree_go2_energy_test as unitree_go2
 from src.algorithms.ppo import network_utilities as ppo_networks
 from src.algorithms.ppo.loss_utilities import loss_function
 from src.distribution_utilities import ParametricDistribution
@@ -37,14 +37,8 @@ flags.DEFINE_string(
 flags.DEFINE_integer(
     'checkpoint_iteration', None, 'Desired checkpoint iteration.', short_name='i',
 )
-flags.DEFINE_bool(
-    'overwrite_metadata', False, 'Overwrite metadata from restored checkpoint.', short_name='o',
-)
 flags.DEFINE_string(
     'tag', '', 'Tag for wandb run.', short_name='t',
-)
-flags.DEFINE_bool(
-    'reinitialize_policy_std', False, 'Reinitialize Policy Output Layer parameters that correlate to Action STD.', short_name='r',
 )
 
 
@@ -148,49 +142,6 @@ def main(argv=None):
             environment=env,
             restore_iteration=FLAGS.checkpoint_iteration,
         )
-
-        if FLAGS.reinitialize_policy_std:
-            # Reset Policy std params to enable exploration:
-            key = jax.random.PRNGKey(42)
-            key, bias_key, kernel_key = jax.random.split(key, num=3)
-            policy_params = restored_checkpoint.train_state.params.policy_params
-            output_params = policy_params['params'][f'dense_{metadata.network_metadata.policy_depth}']
-            params = jax.tree.map(lambda x: jnp.split(x, 2, axis=-1), output_params)
-            bias_shape = jnp.expand_dims(params['bias'][-1], axis=0).shape
-            kernel_shape = params['kernel'][-1].shape
-            bias = jnp.concatenate(
-                [
-                    params['bias'][0],
-                    jax.nn.initializers.lecun_uniform()(
-                        key=bias_key, shape=bias_shape,
-                    ).flatten(),
-                ],
-                axis=-1,
-                dtype=jnp.float32,
-            )
-            kernel = jnp.concatenate(
-                [
-                    params['kernel'][0],
-                    jax.nn.initializers.lecun_uniform()(
-                        key=kernel_key, shape=kernel_shape,
-                    ),
-                ],
-                axis=-1,
-                dtype=jnp.float32,
-            )
-            output_layer = {
-                'bias': bias,
-                'kernel': kernel,
-            }
-            policy_params['params'].update({f'dense_{metadata.network_metadata.policy_depth}': output_layer})
-            restored_checkpoint.train_state.params.replace(policy_params=policy_params)
-
-        if FLAGS.overwrite_metadata:
-            run.config.update({
-                'network_metadata': metadata.network_metadata,
-                'loss_metadata': metadata.loss_metadata,
-                'training_metadata': metadata.training_metadata,
-            })
 
     def progress_fn(iteration, num_steps, metrics):
         print(
