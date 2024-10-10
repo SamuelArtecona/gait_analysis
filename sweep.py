937b504495895c9314e7f9d018ce94cc9590f6fa
jeh15 <jeh15@fsu.edu>
2024-10-10 16:49:24 -0400

    Added cost function. Test scan function.

--- sweep.py
+++ sweep.py
@@ -1,141 +1,15 @@
 from absl import app, flags
+from typing import List, Callable
 import os
 from enum import Enum
-import copy
-
-import jax
-import numpy as np
-
-from brax.io import html
-
-from src.envs import unitree_go2_energy_test as unitree_go2
-from src.algorithms.ppo.load_utilities import load_policy
-
-jax.config.update("jax_enable_x64", True)
-
-FLAGS = flags.FLAGS
-flags.DEFINE_string(
-    'checkpoint_name', None, 'Desired checkpoint folder name to load.', short_name='c',
-)
-flags.DEFINE_integer(
-    'checkpoint_iteration', None, 'Desired checkpoint iteration.', short_name='i',
-)
-
-
-class Feet(Enum):
-    front_left = 0
-    hind_left = 1
-    front_right = 2
-    hind_right = 3
-
-
-def cost(phase, target):
-    # Load from Env:
-    velocity_target = 0.375
-    phase_targets = [0.25, 0.5, 0.75]
-
-    env = unitree_go2.UnitreeGo2Env(
-        velocity_target=velocity_target,
-        filename='unitree_go2/scene_mjx.xml',
-    )
-    reset_fn = jax.jit(env.reset)
-    step_fn = jax.jit(env.step)
-
-    # Load Policy:
-    make_policy, params = load_policy(
-        checkpoint_name=FLAGS.checkpoint_name,
-        restore_iteration=FLAGS.checkpoint_iteration,
-        environment=env,
-    )
-    inference_function = make_policy(params)
-    inference_fn = jax.jit(inference_function)
-
-    # Initialize Simulation:
-    key = jax.random.key(0)
-
-    gaits = []
-    key, subkey = jax.random.split(key)
-    state = reset_fn(subkey)
-
-    num_steps = 1000
-    steady_state_ratio = 0.8
-    states = []
-    first_contact = []
-    contacts = []
-    for i in range(num_steps):
-        key, subkey = jax.random.split(key)
-        action, _ = inference_fn(state.obs, subkey)
-        state = step_fn(state, action)
-        states.append(state.pipeline_state)
-
-        # Get Steady State:
-        steady_state_condition = (
-            (i > int((1.0-steady_state_ratio) * num_steps))
-            & (i <= int(steady_state_ratio * num_steps))
-        )
-        if steady_state_condition:
-            first_contact.append(state.info['first_contact'])
-            contacts.append(state.info['previous_contact'])
-
-    first_contact = np.asarray(first_contact)
-    contacts = np.asarray(contacts)
-
-    # Find Start of Stance Indicies for Dominant Foot: [Right Front]
-    indicies = np.where(first_contact[:, Feet.front_right.value])[0]
-    stride_lengths = np.diff(indicies)
-
-    front_left = []
-    hind_right = []
-    hind_left = []
-    for i in indicies:
-        front_left.append(
-            np.where(first_contact[i:, Feet.front_left.value])[0][0]
-        )
-        hind_right.append(
-            np.where(first_contact[i:, Feet.hind_right.value])[0][0]
-        )
-        hind_left.append(
-            np.where(first_contact[i:, Feet.hind_left.value])[0][0]
-        )
-
-    # Calculate Phases:
-    front_left = np.asarray(front_left)[:-1]
-    hind_right = np.asarray(hind_right)[:-1]
-    hind_left = np.asarray(hind_left)[:-1]
-
-    front_left_phase = front_left / stride_lengths
-    hind_right_phase = hind_right / stride_lengths
-    hind_left_phase = hind_left / stride_lengths
-
-    # # Account for phase offset: Subtract 1.0
-    front_left_phase, hind_right_phase, hind_left_phase = list(
-        map(lambda x: np.where(x > 1.0, x - 1.0, x), [front_left_phase, hind_right_phase, hind_left_phase]),
-    )
-
-    # Account for phase offset: Modulus
-    # front_left_phase, hind_right_phase, hind_left_phase = list(
-    #     map(lambda x: np.where(x > 1.0, x % 1, x), [front_left_phase, hind_right_phase, hind_left_phase]),
-    # )
-
-    avg_front_left_phase, avg_hind_right_phase, avg_hind_left_phase = list(
-        map(lambda x: np.mean(x), [front_left_phase, hind_right_phase, hind_left_phase]),
-    )
-
-    # Optimistic Cost: Find closest phase to target:
-    avg_phases = [avg_front_left_phase, avg_hind_right_phase, avg_hind_left_phase]
-    error = np.sort(avg_phases) - np.asarray(phase_targets)
-    cost = np.sum(np.square(error))
-
-
-from absl import app, flags
-import os
 import functools
 
 import jax
-import jax.numpy as jnp
 import flax.linen as nn
 import distrax
 import optax
+
+import numpy as np
 
 import wandb
 import orbax.checkpoint as ocp
@@ -147,6 +21,7 @@ from src.distribution_utilities import ParametricDistribution
 from src.algorithms.ppo.train import train
 from src.algorithms.ppo import checkpoint_utilities
 from src.algorithms.ppo.load_utilities import load_checkpoint
+from src.algorithms.ppo.network_utilities import PPONetworkParams
 
 os.environ['XLA_FLAGS'] = (
     '--xla_gpu_enable_triton_softmax_fusion=true '
@@ -171,56 +46,55 @@ flags.DEFINE_string(
 )
 
 
-def objective(make_policy, params):
+def objective(
+    velocity_target: float,
+    phase_targets: List[float],
+    make_policy: Callable,
+    params: PPONetworkParams,
+    num_steps: int = 1000,
+    ratio: float = 0.8,
+) -> float:
     class Feet(Enum):
         front_left = 0
         hind_left = 1
         front_right = 2
         hind_right = 3
 
-    # Load from Env:
-    velocity_target = 0.375
-    phase_targets = [0.25, 0.5, 0.75]
-
+    # Create Environment:
     env = unitree_go2.UnitreeGo2Env(
         velocity_target=velocity_target,
         filename='unitree_go2/scene_mjx.xml',
     )
     reset_fn = jax.jit(env.reset)
-    step_fn = jax.jit(env.step)
 
     # Make Inference Function:
-    inference_function = make_policy(params)
-    inference_fn = jax.jit(inference_function)
+    inference_fn = make_policy(params)
 
     # Initialize Simulation:
     key = jax.random.key(0)
-
     key, subkey = jax.random.split(key)
     state = reset_fn(subkey)
 
-    num_steps = 1000
-    steady_state_ratio = 0.8
-    states = []
-    first_contact = []
-    contacts = []
-    for i in range(num_steps):
+    def loop(carry, xs):
+        key, state = carry
         key, subkey = jax.random.split(key)
         action, _ = inference_fn(state.obs, subkey)
-        state = step_fn(state, action)
-        states.append(state.pipeline_state)
+        state = env.step(state, action)
+        data = (state.pipeline_state, state.info['first_contact'])
+        return (key, state), data
 
-        # Get Steady State:
-        steady_state_condition = (
-            (i > int((1.0-steady_state_ratio) * num_steps))
-            & (i <= int(steady_state_ratio * num_steps))
-        )
-        if steady_state_condition:
-            first_contact.append(state.info['first_contact'])
-            contacts.append(state.info['previous_contact'])
+    _, (states, first_contact) = jax.lax.scan(
+        f=loop,
+        init=(key, state),
+        xs=(),
+        length=num_steps,
+    )
 
-    first_contact = np.asarray(first_contact)
-    contacts = np.asarray(contacts)
+    # Slice Data:
+    start_slice, end_slice = int((1-ratio) * num_steps), int(ratio * num_steps)
+    first_contact = np.asarray(
+        first_contact,
+    )[start_slice:end_slice]
 
     # Find Start of Stance Indicies for Dominant Foot: [Right Front]
     indicies = np.where(first_contact[:, Feet.front_right.value])[0]
@@ -249,9 +123,12 @@ def objective(make_policy, params):
     hind_right_phase = hind_right / stride_lengths
     hind_left_phase = hind_left / stride_lengths
 
-    # # Account for phase offset: Subtract 1.0
+    # Account for phase offset: Subtract 1.0
     front_left_phase, hind_right_phase, hind_left_phase = list(
-        map(lambda x: np.where(x > 1.0, x - 1.0, x), [front_left_phase, hind_right_phase, hind_left_phase]),
+        map(
+            lambda x: np.where(x > 1.0, x - 1.0, x),
+            [front_left_phase, hind_right_phase, hind_left_phase],
+        ),
     )
 
     # Account for phase offset: Modulus
@@ -260,7 +137,10 @@ def objective(make_policy, params):
     # )
 
     avg_phases = list(
-        map(lambda x: np.mean(x), [front_left_phase, hind_right_phase, hind_left_phase]),
+        map(
+            lambda x: np.mean(x),
+            [front_left_phase, hind_right_phase, hind_left_phase],
+        ),
     )
 
     # Sorted Cost Error:
@@ -269,7 +149,6 @@ def objective(make_policy, params):
 
     return cost
 
-    
 
 def main(argv=None):
     # Config:
@@ -365,11 +244,6 @@ def main(argv=None):
         filename='unitree_go2/scene_barkour_hfield_mjx.xml',
         config=reward_config,
     )
-    render_env = unitree_go2.UnitreeGo2Env(
-        velocity_target=velocity_target,
-        filename='unitree_go2/scene_barkour_hfield_mjx.xml',
-        config=reward_config,
-    )
 
     def progress_fn(iteration, num_steps, metrics):
         print(
@@ -435,8 +309,6 @@ def main(argv=None):
         randomization_fn=randomization_fn,
         checkpoint_fn=checkpoint_fn,
         wandb=run,
-        render_environment=render_env,
-        render_interval=5,
     )
 
     policy_generator, params, metrics = train_fn(
